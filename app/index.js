2f6852e3dbc1b94b154249a4ec87d444192e3b96
Donatas Petrauskas <donatasp@wix.com>
2016-07-14 11:11:31 +0300

    Initial setup.

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Scramblr extends React.Component {
+  render() {
+    return <div>Hello, World!</div>;
+  }
+}
+
+ReactDOM.render(<Scramblr />, document.getElementById('app'));
---
+++ test/app.spec.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('App', function () {
+  it('should work', function () {
+    expect(1).to.equal(1);
+  });
+});
