68a2aa21f4c3268492b0005cd4c416226f0a85ea
Klaudijus <klaudijusv@wix.com>
2016-07-14 11:56:54 +0300

    Initial Test

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,7 +1,17 @@
 const expect = require('chai').expect;
+const shallow = require('enzyme').shallow;
+
+const React = require('react');
+
+const Test = React.createClass({
+  render() {
+    return <div>Hello</div>;
+  }
+})
 
 describe('App', function () {
   it('should work', function () {
-    expect(1).to.equal(1);
+    const test = shallow(<Test />);
+    expect(test.text()).to.equal('Hello');
   });
 });
